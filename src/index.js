b36aad4f9714f36d0fd015cd7c3fb789ff5d8130
Danny Siu <dannysiu392005@gmail.com>
2018-08-28 19:23:00 +0800

    Add fork me on GitHub link

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
+
 ReactDOM.render((
   <BrowserRouter>
     <App />
